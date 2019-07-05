42bbd4c46c0995edc94d1433373414988442df74
Valek <zer0cool@bk.ru>
2019-07-05 18:51:57 +0300

    Задания на циклы

---
+++ cycles/cycles.js
@@ -0,0 +1,17 @@
+for (let i = 1; i < 8; i++){
+    console.log(i)
+}
+
+var i = 1;
+
+while (i < 8){
+    console.log(i);
+    i++;
+}
+
+var j = 1;
+
+do{
+    console.log(j);
+    j++;
+}while(j < 8);
