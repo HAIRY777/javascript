e92ee9429f0c79b35da96f01d3827c6eb431a4f2
Valek <zer0cool@bk.ru>
2019-07-06 13:33:11 +0300

    Задание на циклы

--- lessons_1/script.js
+++ lessons_1/script.js
@@ -1,20 +1,42 @@
-var money  = prompt("Ваш бюджет на месяц?", "");
+var money  = +prompt("Ваш бюджет на месяц?", "");
 var time = prompt("Введите дату в формате YYYY-MM-DD", "");
-var st = prompt("Введите обязательную статью расходов в этом месяце", "");
-var sum = prompt("Во сколько обойдется?", "");
+
 
 
 
 var appData = {
     budget: money,
     timeData: time,
-    expenses: {
-        st:sum,
-    },
-    optionalExpenses: "",
+    expenses: {},
+    optionalExpenses: {},
     income: [],
     savings: false,
 
 };
 
-alert("Ваш бюджет на 1 день, равен " + appData.budget/30)
+for (let i = 0; i < 2; i++){
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+
+    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null
+        && a != '' && b != '' && a.length < 50){
+        appData.expenses[a] = b;
+    }else{
+        i--;
+        continue;
+    }
+}
+
+appData.MoneyPerDay = appData.budget / 30;
+
+alert('Ежедневный бюджет: ' + appData.MoneyPerDay);
+
+if (appData.MoneyPerDay < 100){
+    console.log("Минимальный уровень достатка");
+}else if (appData.MoneyPerDay > 100 && appData.MoneyPerDay < 2000){
+    console.log("Средний уровень достатка");
+}else if (appData.MoneyPerDay > 2000){
+    console.log("Высокий уровень достатка");
+}else{
+    console.log("Что-то не так")
+}
